0e2f55eb136ae4667aa3d48246f319a973bd41d6
Akash Tyagi <tyagiakash926@gmail.com>
2020-12-21 19:56:32 +0530

    background done

---
+++ facts/background.js
@@ -0,0 +1,14 @@
+// console.log("background");
+chrome.tabs.onCreated.addListener(function(tab){
+    console.log(tab);
+})
+
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    // console.log(changeInfo);
+    // console.log(tabId);
+    // console.log(tab);
+    if(tab.url.includes("youtube")){
+        console.log("youtube");
+        chrome.tabs.remove(tabId);
+    }
+})
