5e8d48d676680d14c2a8dc302c05cd319b3d4ef3
Akash Tyagi <tyagiakash926@gmail.com>
2020-12-26 00:27:24 +0530

    data not send

--- activity/meaningFinder/background.js
+++ activity/meaningFinder/background.js
@@ -1,5 +1,20 @@
-// import axios from 'axios';
-fetch('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=pluto&exchars=500&explaintext=1').then(r => r.text()).then(result => {
+console.log("background");
+let globalResult ="hii";
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if(request.type=="add"){
+        finder(request.site);
+        console.log(globalResult,"insend response")
+        sendResponse(true);
+    } 
+});
+
+function  finder(word){
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${word}&exchars=500&explaintext=1`).then(r => r.json()).then(result => {
     // Result now contains the response text, do what you want...
-    console.log(result);
-})
+    
+    const entries = Object.entries(result.query.pages);
+    console.log(entries[0][1].extract);
+    });
+}
+
+
--- activity/meaningFinder/popup.js
+++ activity/meaningFinder/popup.js
@@ -1,10 +1,17 @@
-const api = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${name}&exchars=500&explaintext=1"
+// const api = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${name}&exchars=500&explaintext=1"
 
 let inputBox = document.querySelector("#search");
-let searchBtn = document.querySelector(".serach-btn");
+let searchBtn = document.querySelector(".search-btn");
 let content = document.querySelector(".content");
-
-inputBox.addEventListener("change",function(){
+let paragraph ;
+searchBtn.addEventListener("click",function(){
+    console.log("HII");
     let value = inputBox.value;
-    console.log(value);
+    if(value){
+        console.log(value);
+        chrome.runtime.sendMessage({type:"add" , site:value}, function(response) {
+            console.log(response);
+            // content.textContent(response);
+        }); 
+    }
 })
